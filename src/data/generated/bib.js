bdcc39d553fc6930169f07bc72029cccee7747f1
sowwww234 <1194678240@qq.com>
2026-05-11 00:23:37 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,111 @@
 ﻿define({ entries : {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "buyya2018cost": {
+        "author": "Buyya, Rajkumar and Rodriguez, Maria A. and Toosi, Adel Nadjaran and Park, Jaeman",
+        "doi": "10.1088/1742-6596/1108/1/012001",
+        "journal": "Journal of Physics: Conference Series",
+        "keywords": "cost, cloud, orchestration",
+        "pages": "012001",
+        "title": "Cost-Efficient Orchestration of Containers in Clouds: A Vision, Architectural Elements, and Future Directions",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
+        "volume": "1108",
+        "year": "2018"
+    },
+    "carrion2022kubernetes": {
+        "author": "Carri\u00f3n, Carmen",
+        "doi": "10.1145/3539606",
+        "journal": "ACM Computing Surveys",
+        "keywords": "survey, scheduling, kubernetes",
+        "number": "7",
+        "pages": "1--37",
+        "title": "Kubernetes Scheduling: Taxonomy, Ongoing Issues and Challenges",
+        "type": "article",
+        "volume": "55",
+        "year": "2022"
+    },
+    "cilic2023performance": {
+        "author": "Cili\u0107, Ivan and Krivi\u0107, Petar and Podnar \u017darko, Ivana and Ku\u0161ek, Mario",
+        "doi": "10.3390/s23084008",
+        "journal": "Sensors",
+        "keywords": "edge, performance, orchestration",
+        "number": "4008",
+        "title": "Performance Evaluation of Container Orchestration Tools in Edge Computing Environments",
+        "type": "article",
+        "volume": "23",
+        "year": "2023"
+    },
+    "fayosjordan2020performance": {
+        "author": "Fayos-Jordan, Rafael and Felici-Castell, Santiago and Segura-Garcia, Jaume and Lopez-Ballester, Jesus and Cobos, Maximo",
+        "doi": "10.1016/j.jnca.2020.102788",
+        "journal": "Journal of Network and Computer Applications",
+        "keywords": "fog, performance, benchmark",
+        "pages": "102788",
+        "title": "Performance comparison of container orchestration platforms with low cost devices in the fog, assisting Internet of Things applications",
+        "type": "article",
+        "volume": "169",
+        "year": "2020"
+    },
+    "medel2016modelling": {
+        "author": "Medel, V\u00edctor and Rana, Omer and Ba\u00f1ares, Jos\u00e9 \u00c1ngel and Arronategui, Unai",
+        "booktitle": "2016 IEEE/ACM 9th International Conference on Utility and Cloud Computing",
+        "doi": "10.1145/2996890.3007869",
+        "keywords": "performance, modelling, resource management",
+        "pages": "257--262",
+        "title": "Modelling Performance and Resource Management in Kubernetes",
+        "type": "inproceedings",
         "year": "2016"
+    },
+    "senjab2023survey": {
+        "author": "Senjab, Khaldoun and Abbas, Sohail and Ahmed, Naveed and Khan, Atta ur Rehman",
+        "doi": "10.1186/s13677-023-00471-1",
+        "journal": "Journal of Cloud Computing",
+        "keywords": "survey, scheduling, kubernetes",
+        "number": "87",
+        "title": "A survey of Kubernetes scheduling algorithms",
+        "type": "article",
+        "volume": "12",
+        "year": "2023"
+    },
+    "spatharakis2022distributed": {
+        "author": "Spatharakis, Dimitrios and Dimolitsas, Ioannis and Vlahakis, Eleftherios and Dechouniotis, Dimitrios and Athanasopoulos, Nikolaos and Papavassiliou, Symeon",
+        "booktitle": "18th International Conference on Network and Service Management",
+        "keywords": "edge, autoscaling, resource management",
+        "pages": "163--169",
+        "title": "Distributed Resource Autoscaling in Kubernetes Edge Clusters",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "turin2023predicting": {
+        "author": "Turin, Gianluca and Borgarelli, Andrea and Donetti, Simone and Damiani, Ferruccio and Johnsen, Einar Broch and Tapia Tarifa, S. Lizeth",
+        "doi": "10.1016/j.jss.2023.111750",
+        "journal": "The Journal of Systems and Software",
+        "keywords": "prediction, resource model, kubernetes",
+        "pages": "111750",
+        "title": "Predicting resource consumption of Kubernetes container systems using resource models",
+        "type": "article",
+        "volume": "203",
+        "year": "2023"
+    },
+    "zhong2020cost": {
+        "author": "Zhong, Zhiheng and Buyya, Rajkumar",
+        "doi": "10.1145/3378447",
+        "journal": "ACM Transactions on Internet Technology",
+        "keywords": "cost, resource management, orchestration",
+        "number": "2",
+        "pages": "1--24",
+        "title": "A Cost-Efficient Container Orchestration Strategy in Kubernetes-Based Cloud Computing Infrastructures with Heterogeneous Resources",
+        "type": "article",
+        "volume": "20",
+        "year": "2020"
+    },
+    "zhou2021container": {
+        "author": "Zhou, Naweiluo and Georgiou, Yiannis and Pospieszny, Marcin and Zhong, Li and Zhou, Huan and Niethammer, Christoph and Pejak, Branislav and Marko, Oskar and Hoppe, Dennis",
+        "doi": "10.1186/s13677-021-00231-z",
+        "journal": "Journal of Cloud Computing",
+        "keywords": "hpc, orchestration, kubernetes",
+        "number": "16",
+        "title": "Container orchestration on HPC systems through Kubernetes",
+        "type": "article",
+        "volume": "10",
+        "year": "2021"
     }
 }});
